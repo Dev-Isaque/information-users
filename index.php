69804b688933686270009ff6f44cb6fa75cf06c6
Dev-Isaque <isaquesbl123@gmail.com>
2024-05-07 21:56:57 -0300

    up

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark">
-      <a class="navbar-brand" href="#">Sistema</a>
+      <a class="navbar-brand" href="#"> Sistema</a>
       <button class="navbar-toggler" type="button" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
